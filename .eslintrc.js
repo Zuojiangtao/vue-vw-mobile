8db42b499ac38c57ec274c58541a8fd9de49d02d
ZuoJiangtao <1799158837@qq.com>
2023-02-22 10:49:15 +0800

    build: 统一代码规范
    添加eslint及prettier相关依赖及配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,43 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/standard"],
-  rules: {},
+  extends: ['plugin:vue/strongly-recommended', '@vue/standard', 'plugin:prettier/recommended'],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
     ecmaFeatures: {
       legacyDecorators: true,
     },
+    requireConfigFile: false,
   },
-  overrides: [
-    {
-      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
-      env: {
-        jest: true,
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0, // 禁止使用debugger
+    'prettier/prettier': [
+      2,
+      {
+        semi: false,
       },
-    },
-  ]
+      {
+        usePrettierrc: true,
+      },
+    ],
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 5,
+        multiline: 1,
+      },
+    ],
+    'vue/attribute-hyphenation': 0,
+    'vue/html-self-closing': 0,
+    'vue/component-name-in-template-casing': 0,
+    'vue/html-closing-bracket-spacing': 0,
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/no-unused-components': 0,
+    'vue/multiline-html-element-content-newline': 0,
+    'vue/no-use-v-if-with-v-for': 0,
+    'vue/html-closing-bracket-newline': 0,
+    'vue/no-parsing-error': 0,
+    'vue/multi-word-component-names': 0,
+    'vue/no-mutating-props': 0,
+  },
 }
